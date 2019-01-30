f52108b0da5931640b6be97c27412291571c7c7c
Bruno Carvalho <bruno580@gmail.com>
2019-01-30 21:49:38 +1100

    aaa

--- shell/aws/setupIAM-Postgres.sh
+++ shell/aws/setupIAM-Postgres.sh
@@ -113,5 +113,6 @@ else
     echo "Environment configured successfully."
     echo "You can connect to the database now with the following connection string: "
     echo "${CONN}"
+    echo "${PGPASSWORD}"
     exit 0
 fi
